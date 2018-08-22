1d643bbf230b11982df66e95afe769a53415326f
Łukasz Gryglicki <lgryglicki@cncf.io>
2018-08-22 12:00:51 +0000

    Get repos related fixes
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

---
+++ git/git_reset_pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  echo "Argument required: path to call git-reset and the git-pull"
+  exit 1
+fi
+
+cd "$1" || exit 2
+git reset --hard || exit 3
+git pull || exit 4
