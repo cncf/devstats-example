1daa69a95f9d65a202f12a24d070b850b4e233de
Łukasz Gryglicki <lgryglicki@cncf.io>
2018-08-24 08:32:18 +0000

    Backport encrypted_email changes from devstats
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- copy_devstats_binaries.sh
+++ copy_devstats_binaries.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-binaries="structure runq gha2db calc_metric gha2db_sync import_affs annotations tags webhook devstats get_repos merge_dbs replacer vars ghapi2db columns hide_data website_data sync_issues sqlitedb"
+binaries="structure runq gha2db calc_metric gha2db_sync import_affs annotations tags webhook devstats get_repos merge_dbs replacer vars ghapi2db columns hide_data website_data sync_issues update_encrypted_email sqlitedb"
 for f in $binaries
 do
   cp $GOPATH/bin/$f ./ || exit 1
