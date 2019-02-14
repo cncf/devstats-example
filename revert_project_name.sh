23e131af46fceb50d894f186e9258f1d40580f7a
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-14 06:16:41 +0000

    Wip replace script 2
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- revert_project_name.sh
+++ revert_project_name.sh
@@ -12,20 +12,4 @@ then
   echo "Usage: $0 lower_case_project_name 'Project Name'"
   exit 2
 fi
-
-to_lowername=$1
-to_fullname=$2
-from_lowername=$3
-from_fullname=$4
-
-if [ -z "$from_lowername" ]
-then
-  from_lowername=homebrew
-fi
-
-if [ -z "$from_fullname" ]
-then
-  from_fullname=Homebrew
-fi
-
-vim -c "%s/$from_lowername/$to_lowername/g|wq" ./grafana.sh
+./set_project_name.sh homebrew Homebrew "$1" "$2"
