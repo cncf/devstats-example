b33cc9bb7eecc9c21ab5d71bef996d943de0255b
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-02-14 06:09:16 +0000

    Wip replace script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- set_project_name.sh
+++ set_project_name.sh
@@ -5,9 +5,15 @@ then
   echo "Usage: $0 lower_case_project_name 'Project Name'"
   exit 1
 fi
+
 if [ -z "$2" ]
 then
   echo "$0: missing second parameter: 'Project Name'"
   echo "Usage: $0 lower_case_project_name 'Project Name'"
   exit 2
 fi
+
+ln=$1
+fn=$2
+
+vim -c '%s/homebrew/$fn/g|wq' ./grafana.sh
