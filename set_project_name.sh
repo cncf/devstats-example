eda83dc77361527d6c6d21aaf8f71660b8a939d9
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-18 14:30:01 +0000

    Fix replace order
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- set_project_name.sh
+++ set_project_name.sh
@@ -66,13 +66,13 @@ from_repo=${from_repo/\//\\\/}
 to_repo=${to_repo/\//\\\/}
 
 vim -c "%s/$from_lowername/$to_lowername/g|wq" ./grafana.sh
+vim -c "%s/'$from_repo'/'$to_repo'/g|wq" ./projects.yaml
 vim -c "%s/$from_lowername/$to_lowername/g|wq" ./projects.yaml
-vim -c "%s/$from_repo/$to_repo/g|wq" ./projects.yaml
 vim -c "%s/$from_date/$to_date/g|wq" ./projects.yaml
 vim -c "%s/- $from_org/- $to_org/g|wq" ./projects.yaml
 vim -c "%s/$from_fullname/$to_fullname/g|wq" ./projects.yaml
+vim -c "%s/\"$from_repo\"/\"$to_repo\"/g|wq" ./devel/deploy_all.sh
 vim -c "%s/$from_lowername/$to_lowername/g|wq" ./devel/deploy_all.sh
-vim -c "%s/$from_repo/$to_repo/g|wq" ./devel/deploy_all.sh
 vim -c "%s/$from_fullname/$to_fullname/g|wq" ./devel/deploy_all.sh
 vim -c "%s/$from_lowername/$to_lowername/g|wq" ./devel/get_all_sqlite_jsons.sh
 vim -c "%s/$from_lowername/$to_lowername/g|wq" ./devel/put_all_charts.sh
