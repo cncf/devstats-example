0389aae0217ef11326c173eba210e8fe9fc3f15c
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-03-18 13:33:36 +0000

    More fixes to msync with the newest code base
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- devel/put_all_charts.sh
+++ devel/put_all_charts.sh
@@ -7,7 +7,7 @@ else
 fi
 for proj in $all
 do
-    suff=$proj
-    NOCOPY=1 GRAFANA=$suff devel/import_jsons_to_sqlite.sh grafana/dashboards/$proj/*.json || exit 2
+  suff=$proj
+  NOCOPY=1 GRAFANA=$suff devel/import_jsons_to_sqlite.sh grafana/dashboards/$proj/*.json || exit 2
 done
 echo 'OK'
