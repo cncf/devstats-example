151344cb98414a06a4bde75945e108403fdf9c0d
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-26 08:31:26 +0000

    Failsafe vim scripts
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- set_project_name.sh
+++ set_project_name.sh
@@ -65,35 +65,35 @@ to_org=${to_arr[0]}
 from_repo=${from_repo/\//\\\/}
 to_repo=${to_repo/\//\\\/}
 
-vim -c "%s/$from_lowername/$to_lowername/g|wq" ./grafana.sh
-vim -c "%s/'$from_repo'/'$to_repo'/g|wq" ./projects.yaml
-vim -c "%s/$from_lowername/$to_lowername/g|wq" ./projects.yaml
-vim -c "%s/$from_date/$to_date/g|wq" ./projects.yaml
-vim -c "%s/- $from_org/- $to_org/g|wq" ./projects.yaml
-vim -c "%s/$from_fullname/$to_fullname/g|wq" ./projects.yaml
-vim -c "%s/\"$from_repo\"/\"$to_repo\"/g|wq" ./devel/deploy_all.sh
-vim -c "%s/$from_lowername/$to_lowername/g|wq" ./devel/deploy_all.sh
-vim -c "%s/$from_fullname/$to_fullname/g|wq" ./devel/deploy_all.sh
-vim -c "%s/$from_lowername/$to_lowername/g|wq" ./devel/get_all_sqlite_jsons.sh
-vim -c "%s/$from_lowername/$to_lowername/g|wq" ./devel/put_all_charts.sh
-vim -c "%s/$from_lowername/$to_lowername/g|wq" ./devel/add_single_metric_all.sh
-vim -c "%s/$from_lowername/$to_lowername/g|wq" ./devel/create_psql_user.sh
-vim -c "%s/$from_lowername/$to_lowername/g|wq" ./devel/vars_all.sh
-vim -c "%s/$from_lowername/$to_lowername/g|wq" ./crontab
-vim -c "%s/$from_lowername/$to_lowername/g|wq" "./$from_lowername/psql.sh"
-vim -c "%s/$from_date/$to_date/g|wq" "./$from_lowername/psql.sh"
-vim -c "%s/$from_org/$to_org/g|wq" "./$from_lowername/psql.sh"
+vim --not-a-term -c "%s/$from_lowername/$to_lowername/g" -c 'wq!' ./grafana.sh
+vim --not-a-term -c "%s/'$from_repo'/'$to_repo'/g" -c 'wq!' ./projects.yaml
+vim --not-a-term -c "%s/$from_lowername/$to_lowername/g" -c 'wq!' ./projects.yaml
+vim --not-a-term -c "%s/$from_date/$to_date/g" -c 'wq!' ./projects.yaml
+vim --not-a-term -c "%s/- $from_org/- $to_org/g" -c 'wq!' ./projects.yaml
+vim --not-a-term -c "%s/$from_fullname/$to_fullname/g" -c 'wq!' ./projects.yaml
+vim --not-a-term -c "%s/\"$from_repo\"/\"$to_repo\"/g" -c 'wq!' ./devel/deploy_all.sh
+vim --not-a-term -c "%s/$from_lowername/$to_lowername/g" -c 'wq!' ./devel/deploy_all.sh
+vim --not-a-term -c "%s/$from_fullname/$to_fullname/g" -c 'wq!' ./devel/deploy_all.sh
+vim --not-a-term -c "%s/$from_lowername/$to_lowername/g" -c 'wq!' ./devel/get_all_sqlite_jsons.sh
+vim --not-a-term -c "%s/$from_lowername/$to_lowername/g" -c 'wq!' ./devel/put_all_charts.sh
+vim --not-a-term -c "%s/$from_lowername/$to_lowername/g" -c 'wq!' ./devel/add_single_metric_all.sh
+vim --not-a-term -c "%s/$from_lowername/$to_lowername/g" -c 'wq!' ./devel/create_psql_user.sh
+vim --not-a-term -c "%s/$from_lowername/$to_lowername/g" -c 'wq!' ./devel/vars_all.sh
+vim --not-a-term -c "%s/$from_lowername/$to_lowername/g" -c 'wq!' ./crontab
+vim --not-a-term -c "%s/$from_lowername/$to_lowername/g" -c 'wq!' "./$from_lowername/psql.sh"
+vim --not-a-term -c "%s/$from_date/$to_date/g" -c 'wq!' "./$from_lowername/psql.sh"
+vim --not-a-term -c "%s/$from_org/$to_org/g" -c 'wq!' "./$from_lowername/psql.sh"
 mv "./$from_lowername" "./$to_lowername" || exit 5
 mv "grafana/img/${from_lowername}.svg" "grafana/img/${to_lowername}.svg" || exit 6
 mv "grafana/img/${from_lowername}32.png" "grafana/img/${to_lowername}32.png" || exit 7
-vim -c "%s/$from_lowername/$to_lowername/g|wq" "./grafana/$from_lowername/change_title_and_icons.sh"
-vim -c "%s/$from_fullname/$to_fullname/g|wq" "./grafana/$from_lowername/change_title_and_icons.sh"
-vim -c "%s/$from_lowername/$to_lowername/g|wq" "./grafana/$from_lowername/grafana_start.sh"
+vim --not-a-term -c "%s/$from_lowername/$to_lowername/g" -c 'wq!' "./grafana/$from_lowername/change_title_and_icons.sh"
+vim --not-a-term -c "%s/$from_fullname/$to_fullname/g" -c 'wq!' "./grafana/$from_lowername/change_title_and_icons.sh"
+vim --not-a-term -c "%s/$from_lowername/$to_lowername/g" -c 'wq!' "./grafana/$from_lowername/grafana_start.sh"
 mv "./grafana/$from_lowername" "./grafana/$to_lowername" || exit 8
 MODE=ss0 FROM="\"$from_lowername\"" TO="\"$to_lowername\"" FILES=`find "./grafana/dashboards/$from_lowername/" -type f -iname '*.json'` ./devel/mass_replace.sh
 MODE=ss0 FROM="\"$from_fullname\"" TO="\"$to_fullname\"" FILES=`find "./grafana/dashboards/$from_lowername/" -type f -iname '*.json'` ./devel/mass_replace.sh
 mv "./grafana/dashboards/$from_lowername" "./grafana/dashboards/$to_lowername" || exit 9
-vim -c "%s/$from_lowername/$to_lowername/g|wq" "./metrics/$from_lowername/vars.yaml"
-vim -c "%s/$from_fullname/$to_fullname/g|wq" "./metrics/$from_lowername/vars.yaml"
+vim --not-a-term -c "%s/$from_lowername/$to_lowername/g" -c 'wq!' "./metrics/$from_lowername/vars.yaml"
+vim --not-a-term -c "%s/$from_fullname/$to_fullname/g" -c 'wq!' "./metrics/$from_lowername/vars.yaml"
 mv "./metrics/$from_lowername" "./metrics/$to_lowername" || exit 10
 mv "./scripts/$from_lowername" "./scripts/$to_lowername" || exit 11
